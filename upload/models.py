7500b9a33e81fe6992c437c6b9fac46d20b04bce
Waldemar Kornewald <wkornewald>
2010-06-10 23:27:50 +0200

    added a nicer file name

--- upload/models.py
+++ upload/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class StorageModel(models.Model):
     file = models.FileField(upload_to='uploads/%Y/%m/%d/%H/%M/%S/')
+
+    @property
+    def filename(self):
+        return self.file.name.rsplit('/', 1)[-1]
