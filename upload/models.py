57f63967f544e8c76017855d036039dd2a72b62d
adieu <adieu@adieu.cn>
2011-03-02 13:43:11 +0800

    display thumbnail for uploaded images

--- upload/models.py
+++ upload/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from google.appengine.api.images import get_serving_url
+
 class UploadModel(models.Model):
     title = models.CharField(max_length=64, blank=True)
     file = models.ImageField(upload_to='uploads/%Y/%m/%d/%H/%M/%S/')
@@ -7,3 +9,7 @@ class UploadModel(models.Model):
     @property
     def filename(self):
         return self.file.name.rsplit('/', 1)[-1]
+
+    @property
+    def image_thumbnail_url(self):
+        return get_serving_url(str(self.file.file.blobstore_info.key()), 200)
